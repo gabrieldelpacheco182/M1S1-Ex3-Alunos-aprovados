8b3b3fd755cae2d2c6e1f71081f661ada79ba0c5
gabrieldelpacheco182 <gabriel_o_pacheco@estudante.sesisenai.org.br>
2023-02-14 08:58:01 -0300

    aprovado e reprovado utilizando a estrutura for

---
+++ function.js
@@ -0,0 +1,58 @@
+let students = [
+    {
+        name: "Gabriel",
+        height: 1.77,
+        age: 36,
+        sex: "Masculino",
+        room: "Trindade",
+        mathematics: 5,
+    },
+    {
+        name: "Adriana",
+        height: 1.657,
+        age: 426,
+        sex: "Feminino",
+        room: "Coaria",
+        mathematics: 7,
+    },
+    {
+        name: "Paula",
+        height: 1.7,
+        age: 25,
+        sex: "Feminino",
+        room: "Palhoça",
+        mathematics: 3,
+    },
+    {
+        name: "Leonardo",
+        height: 1.83,
+        age: 21,
+        sex: "Masculino",
+        room: "Palhoça",
+        mathematics: 8,
+    },
+    {
+        name: "Seloé",
+        height: 1.63,
+        age: 70,
+        sex: "Masculino",
+        room: "Laguna",
+        mathematics: 4,
+    },
+];
+
+if (students[0].mathematics < 7) {
+    alert("Reprovado");
+} else alert("Aprovado");
+if (students[1].mathematics < 7) {
+    alert("Reprovado");
+} else alert("Aprovado");
+if (students[2].mathematics < 7) {
+    alert("Reprovado");
+} else alert("Aprovado");
+if (students[3].mathematics < 7) {
+    alert("Reprovado");
+} else alert("Aprovado");
+if (students[4].mathematics < 7) {
+    alert("Reprovado");
+} else alert("Aprovado");
